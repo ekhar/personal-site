f6d86f8df28da873f80f53d455ae1ae89aef99a8
Eric Khar <ericsrealemail@gmail.com>
2024-09-04 02:03:09 -0400

    blog title works

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import { formatDate } from '$lib/utils';
 	export let data;
 </script>
 
 <article>
-	<h1>Blog</h1>
+	<h2>Recent Blog Posts</h2>
+	<div class="posts-grid">
+		{#each data.posts as post}
+			<div class="post-card">
+				<h3><a href="/blog/{post.slug}">{post.title}</a></h3>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+				<a href="/blog/{post.slug}" class="read-more">Read More</a>
+			</div>
+		{/each}
+	</div>
 </article>
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,7 +1,7 @@
-import type { PageLoad } from './$types';
+import type { Post } from '$lib/types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/posts');
-	const posts = await response.json();
+export async function load({ fetch }) {
+	const response = await fetch('api/posts');
+	const posts: Post[] = await response.json();
 	return { posts };
-};
+}
